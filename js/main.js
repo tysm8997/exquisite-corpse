ffb45ee07a4e6e09c3aab9466fd0ad5b69e4bee0
Kimberly Preston <kimberlypreston@Kimberlys-MacBook-Pro-2.local>
2017-02-14 15:39:40 -0700

    initial parallax draft

--- js/main.js
+++ js/main.js
@@ -1,10 +1,39 @@
+$(document).ready(function(){
+
+
 
 $(".button").click(function(){
-		$("body").animate({ scrollTop: 675 }, 'slow');
+		$("body").animate({ scrollTop: 700 }, 'slow');
 
 
 });
 
+     $(window).scroll(function(){
+          
+        parallax();
+       
+
+    });
+
+
+
+});
+
+function parallax(){
+        var scrolled = $(window).scrollTop();
+        
+        // if(scrolled < 700){
+        $('.tacoback').css('top', (scrolled * 0.45));
+
+
+
+        $('.story').css('top', -900 + (scrolled * 0.1));
+
+    // }
+
+
+
+}
 
 
 
