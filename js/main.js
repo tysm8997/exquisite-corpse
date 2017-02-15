d282ce05bbd9478ba231754f3b74380f4ac6dbd7
Kimberly Preston <kimberlypreston@Kimberlys-MacBook-Pro-2.local>
2017-02-15 15:40:15 -0700

    edits

--- js/main.js
+++ js/main.js
@@ -16,12 +16,13 @@ $(".button").click(function(){
       var scroll = $(window).scrollTop();
 
 	if(scroll > 1800){
-		// $(".two").css("opacity",  (1.0));
-		// $(".two").fadeIn("slow");
 		$(".two").fadeTo("slow", 1.0);
 	}
-	if(scroll > 1900){
+	if(scroll > 1950){
 		$(".three").fadeTo("slow", 1.0);
+		$(".four").delay(1000).fadeTo("slow", 1.0);
+		$(".five").delay(3000).fadeTo("slow", 1.0);
+		$(".six").delay(5000).fadeTo("slow", 1.0);
 	}       
 
     });
